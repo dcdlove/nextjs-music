f9af11669c4829807028a5ce2e8ad7682cbdf748
dcdlove <dcdlove@qq.com>
2025-05-07 19:36:57 +0800

    fix: 修正音乐文件请求的URL路径

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   //请求文件流转换成提供给给audio标签播放的url
   const [audioUrl, setAudioUrl] = useState<string | null>(null)
   const handleClick = async () => {
-    const response = await fetch("http://localhost:3000/%E4%B8%83%E5%85%AC%E4%B8%BB-%E6%96%BD%E5%B1%95%E5%92%92%E8%AF%AD.lkmp3", {
+    const response = await fetch("./%E4%B8%83%E5%85%AC%E4%B8%BB-%E6%96%BD%E5%B1%95%E5%92%92%E8%AF%AD.lkmp3", {
       method: "GET",
       headers: {
         "Content-Type": "audio/mpeg",
