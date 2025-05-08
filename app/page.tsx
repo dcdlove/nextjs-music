8c97625e2cc49acfd0d0e0df22d5e1e5d166aa38
dcdlove <dcdlove@qq.com>
2025-05-08 09:43:34 +0800

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,79 @@
 'use client'
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Home() {
 
-  //请求文件流转换成提供给给audio标签播放的url
-  const [audioUrl, setAudioUrl] = useState<string | null>(null)
-  const handleClick = async () => {
-    const response = await fetch("https://cdn.jsdelivr.net/gh/dcdlove/nextjs-music@main/public/%E4%B8%83%E5%85%AC%E4%B8%BB-%E6%96%BD%E5%B1%95%E5%92%92%E8%AF%AD.lkmp3", {
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const [audioUrl, setAudioUrl] = useState<string>("https://cdn.jsdelivr.net/gh/dcdlove/nextjs-music@main/public/%E4%B8%83%E5%85%AC%E4%B8%BB-%E6%96%BD%E5%B1%95%E5%92%92%E8%AF%AD.lkmp3")
+
+  const [playlist, setPlaylist] = useState<{ singer: string; title: string; ext: string, url: string }[]>([])
+
+  // 获取请求数据
+  const getMusicData = async () => {
+    const res = await fetch("./data.json", {
       method: "GET",
       headers: {
-        "Content-Type": "audio/mpeg",
+        "Content-Type": "application/json",
       },
     })
-    const blob = await response.blob()
-    const url = URL.createObjectURL(blob)
-    setAudioUrl(url)
-    //自动播放
-    const audio = document.querySelector("audio")
-    if (audio) {
-      audio.play()
-    }
-    //释放url对象
-    return () => {
-      URL.revokeObjectURL(url)
-    }
+    //处理响应json 对象
+
+    const data = await res.json()
+    const list = data.rows.map((n: { singer: string; title: string; ext: string, url: string }) => ({ ...n, url: encodeURIComponent(`https://cdn.jsdelivr.net/gh/dcdlove/past@main/music/${n.singer}-${n.title}${n.ext}`) }))
+    setPlaylist(list)
+    console.log(list)
+
+  }
+  const handleEnded = () => {
+    const currentIndex = playlist.findIndex(item => decodeURIComponent(item.url) === audioUrl)
+    const nextIndex = (currentIndex + 1) % playlist.length // 循环播放
+    setAudioUrl(decodeURIComponent(playlist[nextIndex].url))
   }
 
 
+  useEffect(() => {
+    getMusicData()
+  }, [])
+
+  useEffect(() => {
+    if (audioRef.current && audioUrl) {
+      audioRef.current.play().catch(err => {
+        // 可选处理错误（如浏览器未允许自动播放）
+        console.error('播放失败：', err)
+      })
+    }
+  }, [audioUrl])
+
   return (
-    <div>
-      <button onClick={handleClick} className="cursor-pointer">加载音乐</button>
-      <audio controls src={audioUrl ? audioUrl : undefined}></audio>
-       
+    <div className="max-w-xl mx-auto p-4">
+      <audio
+        ref={audioRef}
+        controls
+        src={audioUrl}
+        className="w-full mb-4 rounded shadow"
+        onEnded={handleEnded}
+      />
+
+      <ul className="bg-white rounded shadow divide-y max-h-[700px] overflow-y-auto">
+        {
+          playlist.map((item, index) => {
+            const isPlaying = audioUrl === decodeURIComponent(item.url)
+            return (
+              <li
+                key={index}
+                onClick={() => setAudioUrl(decodeURIComponent(item.url))}
+                className={`flex items-center gap-2 p-3 cursor-pointer hover:bg-gray-100 transition ${isPlaying ? 'bg-blue-50 text-blue-600 font-semibold' : ''
+                  }`}
+              >
+                {isPlaying ? '🔊' : '🎵'} {item.singer} - {item.title}
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
+
+
 }
